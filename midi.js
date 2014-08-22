09e7c96f3981a9f1f9bcf2d28544be4a75d42ceb
koturn <jeak.koutan.apple@gmail.com>
2014-08-23 02:55:05 +0900

    Fix some points

--- midi.js
+++ midi.js
@@ -1,91 +1,51 @@
-var midi = null;  // グローバルMIDIAccessオブジェクト
-var inputSelect = null;
-var inputList = null;
+var midi = null;
 var outputSelect = null;
 var outputList = null;
 
 try {
   if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess({sysex:false}).then(function(midiAccess) {
-      dispMessage('MIDI ready!');
+    navigator.requestMIDIAccess({sysex: false}).then(function(midiAccess) {
       midi = midiAccess;
 
-      var inputCmb = document.getElementById('inputPort');
-      var inLength = midi.inputs().length;
-      inputList = midi.inputs();
-      for (var i = 0; i < inLength; i++) {
-        var input = midi.inputs()[i];
-        inputCmb.appendChild(new Option(input.manufacturer + ' ' + input.name, input.id ));
-      }
-      document.getElementById('inCount').innerHTML = inLength;
-
       var outputCmb = document.getElementById('outputPort');
-      var outLength = midi.outputs().length;
       outputList = midi.outputs();
-      for ( var i = 0; i < outLength; i++ ) {
-        var output = midi.outputs()[i];
-        outputCmb.appendChild(new Option(output.manufacturer + ' ' + output.name, output.id ))
+      for ( var i = 0; i < outputList.length; i++ ) {
+        var output = outputList[i];
+        outputCmb.appendChild(new Option(output.manufacturer + ' ' + output.name, output.id ));
       }
-      document.getElementById('outCount').innerHTML = outLength;
+      outputCmb.appendChild(new Option('NONE', -1));
 
-      changeInput();
-      changeOutput();
+      if (outputList.length > 0) changeOutput();
     },
     function(msg) {
-      dispMessage('Failed to get MIDI access - ' + msg );
+      alert('Failed to get MIDI access - ' + msg );
     });
   } else {
-    dispMessage('お使いのブラウザはWeb MIDI APIに対応していません。Web MIDI APIを有効化したChromeを使用してください。');
+    alert('お使いのブラウザはWeb MIDI APIに対応していません。Web MIDI APIを有効化したChromeを使用してください。');
   }
 } catch (e) {
-  dispMessage('お使いのブラウザはWeb MIDI APIに対応していません。Web MIDI APIを有効化したChromeを使用してください。');
+  console.log(e);
 }
 
 
 function echoMIDIMessage(event) {
   if (outputSelect) {
     try {
-      outputSelect.send( event.data );
-      dispMessage('MIDI IN : ' + event.data[0].toString(16)
-                  + ' ' + event.data[1].toString(16)
-                  + ' ' + event.data[2].toString(16));
+      outputSelect.send(event.data);
     } catch (e) {
-      dispMessage(e);
+      console.log(e);
     }
   }
 }
 
 
-// var before = [0x80, 60, 0x7f];
 function echoMIDIMessage2(data0, data1) {
   if (outputSelect) {
     try {
       outputSelect.send([data0, data1, 0x7f]);
       outputSelect.send([0x80, data1, 0x7f], performance.now() + 1000.0);
-      dispMessage( 'MIDI IN : ' + data0.toString(16)
-                  + ' ' + data1.toString(16)
-                  + ' ' + 0x7f.toString(16) );
     } catch(e) {
-      dispMessage(e);
-    }
-  }
-}
-
-
-
-
-function changeInput() {
-  var inputCmb = document.getElementById('inputPort');
-  var selectIndex = inputCmb.selectedIndex;
-  var inLength = inputList.length;
-
-  for (var i = 0; i < inLength; i++) {
-    var input = inputList[i];
-    if (inputCmb.options[selectIndex].value == input.id) {
-      inputSelect = input;
-      inputSelect.onmidimessage = echoMIDIMessage;
-    } else {
-      input.onmidimessage = null;
+      console.log(e);
     }
   }
 }
@@ -95,21 +55,13 @@ function changeOutput() {
   var outputCmb = document.getElementById('outputPort');
   var selectIndex = outputCmb.selectedIndex;
   var outLength = outputList.length;
+
+  outputSelect = null;
   for (var i = 0; i < outLength; i++) {
     var output = outputList[i];
     if  (outputCmb.options[selectIndex].value == output.id) {
       outputSelect = output;
       break;
     }
-  }
-}
-
-
-function dispMessage(message){
-  var messageArea = document.getElementById('messageArea');
-  if (messageArea) {
-    messageArea.innerHTML = message;
-  } else {
-    alert(message);
   }
 }
